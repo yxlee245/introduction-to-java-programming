205bbf11a4bfa0188f67fbab416127548e47ee45
yxlee245 <41932856+yxlee245@users.noreply.github.com>
2019-01-04 17:32:38 +0800

    Chapter 14 codes part 1
    Chapter 14 - JavaFX Basics

---
+++ MyJavaFX.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MyJavaFX extends Application {
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        // Create a scene and place a button in the scene
+        Button btOk = new Button("OK");
+        Scene scene = new Scene(btOk, 200, 250);
+        primaryStage.setTitle("MyJavaFX"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    /**
+     * The main method is only needed for the IDE with limited
+     * JavaFX support. Not needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
